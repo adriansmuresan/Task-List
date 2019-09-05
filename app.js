070e79f3f00634a1e8244cb7ba60b5131e659ed1
Adrian S MUresan <adriansmuresan@gmail.com>
2019-09-05 16:12:31 -0500

    Adds the getTasks function that loops through all the tasks and creates the dom elements

--- app.js
+++ app.js
@@ -27,6 +27,37 @@ function loadEventListeners() {
   document.addEventListener('DOMContentLoaded', getTasks);
 }
 
+// Get tasks from local storage
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task) {
+    // Create li element
+  const li = document.createElement('li');
+  // Add class to the li
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(task));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class=" fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  });
+}
+
 // Add Task
 function addTask(e) {
   if(taskInput.value === '') {
