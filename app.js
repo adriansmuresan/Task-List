cffbaefe0cecbf7eb43af95ba085f14a56f052d3
Adrian S MUresan <adriansmuresan@gmail.com>
2019-09-05 15:57:55 -0500

    Adds a function to store the task into the local storage

--- app.js
+++ app.js
@@ -48,10 +48,26 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in Local Storage
+  storeTaskInLocalStorage(taskInput.value);
   // Clear input
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Store Task function
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Function Remove task
